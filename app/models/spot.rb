783d9f02f9774dd2f9ffb919cb682c885ad03e9a
EC2 Default User <ec2-user@ip-172-31-11-23.ap-northeast-1.compute.internal>
2022-09-12 11:35:08 +0000

    fix favorits

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -4,20 +4,21 @@ class Spot < ApplicationRecord
     belongs_to :genre
     has_many :comments, dependent: :destroy
     has_many :favorites, dependent: :destroy
-  
+
   #一覧画面でのソート機能
   scope :latest, -> {order(created_at: :desc)}
   scope :old, -> {order(created_at: :asc)}
   scope :star_count, -> {order(star: :desc)}
-  
+
   #いいね機能
   def favorited_by?(user)
+    return false if user == nil
     favorites.exists?(user_id: user.id)
   end
-  
+
   #一覧画面でのキーワード検索機能
-  def self.search(search_word) 
-   Spot.where(["name LIKE(?) OR caption LIKE(?) OR address LIKE(?)", 
+  def self.search(search_word)
+   Spot.where(["name LIKE(?) OR caption LIKE(?) OR address LIKE(?)",
                  "%#{search_word}%", "%#{search_word}%", "%#{search_word}%" ])
   end
 end
